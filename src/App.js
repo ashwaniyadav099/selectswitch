3e470187f4cfeac3207a2128f4d25712d4b7039e
Ashwani Yadav <yadavashwani099@gmail.com>
2023-07-01 18:06:18 +0530

    first comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+ const [citys,setcitys]=useState(["city","setcity"])
+ const handelchange=(e)=>{
+   data.forEach((data)=>{
+    if(data.id === e.target.value){
+      setcitys(data.city)
+    }
+   })
+}
+const data=[
+  {
+    country:"india",
+    id:"ind",
+    city:['dehli','mumbai']
+  },
+  {
+    country:"pakistan",
+    id:"pak",
+    city:['karachi','lohor']
+  },
+  {
+    country:"nepal",
+    id:"nep",
+    city:['katmandu','gaay']
+  }
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div>
+    <select onChange={handelchange}>
+    {  data.map((val,index)=>{
+      return <option value={val.id}>{val.country}</option>
+      })}
+    </select>
+    <select>
+      {
+        citys.map((val,index)=>{
+          return <option value={index}>{val}</option>
+        })
+      }
+    </select>
     </div>
-  );
+  </>
+  )
 }
 
 export default App;
